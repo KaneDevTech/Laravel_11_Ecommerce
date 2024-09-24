83fc020f8ba5ba71ace444a4b94b431e4c7632a4
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2024-09-24 13:54:56 +0700

    Day 0 : Exceptions (FIX)

--- app/Traits/PreventDemoModeChanges.php
+++ app/Traits/PreventDemoModeChanges.php
@@ -11,143 +11,138 @@ trait PreventDemoModeChanges
         return env('DEMO_MODE') == 'On' ? true : false;
     }
 
-    /**
-     * Throws ReadOnlyException on create
-     * @param array $attributes
-     * @throws ReadOnlyException
-     * @deprecated Method not allowed on read-only model
-     */
-    public static function create(array $attributes = [])
-    {
-        if (static::isActive()) {
-             flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-             throw new Redirectingexception();
+    // /**
+    // public static function create(array $attributes = [])
+    // {
+    //     if (static::isActive()) {
+    //          flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //          throw new Redirectingexception();
              
-        }
-        return parent::create($attributes);
-    }
+    //     }
+    //     return parent::create($attributes);
+    // }
 
 
-    public static function forceCreate(array $attributes)
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public static function forceCreate(array $attributes)
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::forceCreate($attributes);
-    }
+    //     }
+    //     return parent::forceCreate($attributes);
+    // }
 
 
-    public function save(array $options = [])
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public function save(array $options = [])
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::save($options);
-    }
+    //     }
+    //     return parent::save($options);
+    // }
 
 
-    public function update(array $attributes = [], array $options = [])
-    {
+    // public function update(array $attributes = [], array $options = [])
+    // {
 
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
              
-        }
-        return parent::update($attributes, $options);
-    }
+    //     }
+    //     return parent::update($attributes, $options);
+    // }
 
 
-    public static function firstOrCreate(array $attributes, array $values = [])
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public static function firstOrCreate(array $attributes, array $values = [])
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::firstOrCreate( $attributes,  $values);
-    }
+    //     }
+    //     return parent::firstOrCreate( $attributes,  $values);
+    // }
 
 
-    public static function firstOrNew(array $attributes, array $values = [])
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public static function firstOrNew(array $attributes, array $values = [])
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::firstOrNew( $attributes,  $values);
-    }
+    //     }
+    //     return parent::firstOrNew( $attributes,  $values);
+    // }
 
 
-    public static function updateOrCreate(array $attributes, array $values = [])
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public static function updateOrCreate(array $attributes, array $values = [])
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::updateOrCreate( $attributes,  $values );
-    }
+    //     }
+    //     return parent::updateOrCreate( $attributes,  $values );
+    // }
 
 
-    public function delete()
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public function delete()
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::delete();
-    }
+    //     }
+    //     return parent::delete();
+    // }
 
  
-    public static function destroy($ids)
-    {
-        if (static::isActive()) {
+    // public static function destroy($ids)
+    // {
+    //     if (static::isActive()) {
             
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::destroy($ids);
-    }
+    //     }
+    //     return parent::destroy($ids);
+    // }
 
 
-    public function push()
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public function push()
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::push();
-    }
+    //     }
+    //     return parent::push();
+    // }
 
 
-    public function insert()
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public function insert()
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::insert();
-    }
+    //     }
+    //     return parent::insert();
+    // }
 
 
-    public function truncate()
-    {
-        if (static::isActive()) {
-            flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
-            throw new Redirectingexception();
+    // public function truncate()
+    // {
+    //     if (static::isActive()) {
+    //         flash_message(translate('Data chaning action is not allowed in demo mode.','warning'));
+    //         throw new Redirectingexception();
             
-        }
-        return parent::truncate();
-    }
+    //     }
+    //     return parent::truncate();
+    // }
 }
